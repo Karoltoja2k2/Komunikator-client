9bb7a6541e6e537eccd4f93d3dce09f064aecfcc
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2019-12-22 00:24:21 +0100

    one way messages, connect, disconnect

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,28 +22,55 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private byte[] buffer;
+        private Socket socket;
+        private bool connected = false;
+        private string connectionKey;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Send_Msg(object sender, RoutedEventArgs e)
         {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint endPoint =  new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
-            socket.Connect(endPoint);
+            if (socket != null)
+            {
+                string msg = messageInput.Text;
+                if (!String.IsNullOrEmpty(msg))
+                {
+                    buffer = Encoding.Default.GetBytes(msg);
+                    socket.Send(buffer, 0, buffer.Length, 0);
+                    messageInput.Text = "";
+                }
+            }
+        }
 
-            string msg = message.Text;
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            if (connected == false)
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+                socket.Connect(endPoint);
+                buffer = new byte[255];
+                int rec = socket.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, rec);
+                connectionKey = Encoding.Default.GetString(buffer);
+            }
+            else
+            {
+                buffer = Encoding.Default.GetBytes(connectionKey);
+                socket.Send(buffer, 0, buffer.Length, 0);
+                socket.Close();
+            }
 
-            byte[] buffer = Encoding.Default.GetBytes(msg);
-            socket.Send(buffer, 0, buffer.Length, 0);
+            connected = !connected;
 
-            buffer = new byte[255];
-            int rec = socket.Receive(buffer, 0, buffer.Length, 0);
-            Array.Resize(ref buffer, rec);
-            Console.WriteLine("Received: {0}", Encoding.Default.GetString(buffer));
-
-            socket.Close();
+            // byte[] buffer = new byte[255];
+            // int rec = socket.Receive(buffer, 0, buffer.Length, 0);
+            // Array.Resize(ref buffer, rec);
         }
 
     }
