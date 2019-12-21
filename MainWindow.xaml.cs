d4f12b02f7498798f8a65179b0063aa851151967
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2019-12-21 17:35:23 +0100

    basic socet client

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +26,25 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint =  new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+            socket.Connect(endPoint);
+
+            string msg = message.Text;
+
+            byte[] buffer = Encoding.Default.GetBytes(msg);
+            socket.Send(buffer, 0, buffer.Length, 0);
+
+            buffer = new byte[255];
+            int rec = socket.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received: {0}", Encoding.Default.GetString(buffer));
+
+            socket.Close();
+        }
+
     }
 }
